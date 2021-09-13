e5394af2957628d07d33eb859b8194f24d61c14a
Ygor Sad <ysads2010@gmail.com>
2021-09-12 21:09:36 -0300

    Create a styling module and add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
 import Router from "./components/Router";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  return (
+  let [fontsLoaded] = useFonts({
+    GaramondBold: require("./assets/fonts/CormorantGaramond-Bold.ttf"),
+    GaramondBoldItalic: require("./assets/fonts/CormorantGaramond-BoldItalic.ttf"),
+    SourceSans: require("./assets/fonts/SourceSansPro.ttf"),
+    SourceSansItalic: require("./assets/fonts/SourceSansPro-Italic.ttf"),
+    SourceSansBold: require("./assets/fonts/SourceSansPro-Bold.ttf"),
+  });
+
+  return !fontsLoaded ? (
+    <AppLoading />
+  ) : (
     <NavigationContainer>
       <Router />
     </NavigationContainer>
---
+++ styles.ts
@@ -0,0 +1,32 @@
+export const Color = {
+  darkGreen: "#3c9c90",
+  darkPink: "#ff736b",
+  gray: "#919191",
+  green: "#d6f2e6",
+  navyBlue: "#292c51",
+  pink: "#fcdcdb",
+  white: "#fff",
+};
+
+export const Font = {
+  garamond: {
+    bold: "GaramondBold",
+    boldItalic: "GaramondBoldItalic",
+  },
+  sourceSans: {
+    regular: "SourceSans",
+    bold: "SourceSansBold",
+    italic: "SourceSansItalic",
+  },
+};
+
+export const FontSize = {
+  body: 16,
+  title1: 36,
+  title2: 24,
+};
+
+export const Radius = {
+  small: 8,
+  large: 24,
+};
